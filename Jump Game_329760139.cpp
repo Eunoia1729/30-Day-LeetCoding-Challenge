18c0274137d6319c536a7e0a4217e3cb4b0d9bbe
eunoia_1729 <bkumarraju@gmail.com>
2020-04-30 20:07:51 +0530

    Remove duplicates

--- Jump Game_329760139.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
-        int i, maxx = 0;
-        for(i = 0; i <= min(maxx,(int) nums.size() - 1); ++i)
-            maxx = max(maxx, nums[i] + i);
-        return (i == nums.size()); 
-    }
-};
